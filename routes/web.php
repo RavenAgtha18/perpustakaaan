bc56a40c307c865a0d7704ef4d3e95e57168d2cb
ravenagatha18 <ravenagatha18@gmail.com>
2023-03-17 10:04:38 +0700

    salah

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TransactionController;
+use App\Models\Member;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Notifications\TransactionNotification;
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,26 +28,41 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+
 Route::get('/catalogs', [App\Http\Controllers\CatalogController::class, 'index']);
-Route::get('/publishers', [App\Http\Controllers\PublisherController::class, 'index']);
-Route::get('/transactions', [App\Http\Controllers\TransactionController::class, 'index']);
-Route::get('/members', [App\Http\Controllers\MemberController::class, 'index']);
-
-
 Route::get('/catalogs/create', [App\Http\Controllers\CatalogController::class, 'create']);
 Route::post('/catalogs', [App\Http\Controllers\CatalogController::class, 'store']);
 Route::get('/catalogs/{catalog}/edit', [App\Http\Controllers\CatalogController::class, 'edit']);
 Route::put('/catalogs/{catalog}', [App\Http\Controllers\CatalogController::class, 'update']);
 Route::delete('/catalogs/{catalog}', [App\Http\Controllers\CatalogController::class, 'destroy']);
 
-
 Route::resource('/authors', App\Http\Controllers\AuthorController::class);
 Route::resource('/publishers', App\Http\Controllers\PublisherController::class);
-Route::resource('/members', App\Http\Controllers\MemberController::class);
 Route::resource('/books', App\Http\Controllers\BookController::class);
+Route::resource('/members', App\Http\Controllers\MemberController::class);
+Route::resource('/transactions', App\Http\Controllers\TransactionController::class);
+Route::get('test_spatie', [App\Http\Controllers\AdminController::class, 'test_spatie']);
 
+Route::get('api/authors', [App\Http\Controllers\AuthorController::class, 'api']);
+Route::get('api/publishers', [App\Http\Controllers\PublisherController::class, 'api']);
+Route::get('api/books', [App\Http\Controllers\BookController::class, 'api']);
+Route::get('api/members', [App\Http\Controllers\MemberController::class, 'api']);
+Route::get('api/transactions', [App\Http\Controllers\TransactionController::class, 'api']);
 
-route::get('/api/authors', [App\Http\Controllers\AuthorController::class, 'api']);
-route::get('/api/publishers', [App\Http\Controllers\PublisherController::class, 'api']);
-route::get('/api/members', [App\Http\Controllers\MemberController::class, 'api']);
-route::get('/api/books', [App\Http\Controllers\BookController::class, 'api']);
+Route::get('/spatie', function () {
+    $role = Spatie\Permission\Models\Role::whereName('officer')->exists() ? Spatie\Permission\Models\Role::whereName('officer')->first() : Spatie\Permission\Models\Role::create(['name' => 'officer']);
+    $permission = Spatie\Permission\Models\Permission::where(['name' => 'index transactions'])->exists() ?  Spatie\Permission\Models\Permission::where(['name' => 'index transactions'])->first() : Spatie\Permission\Models\Permission::create(['name' => 'index transactions']);
+
+    $role->givePermissionTo($permission);
+    $permission->assignRole($role);
+
+    $user = auth()->user();
+    $user->assignRole('officer');
+
+    $user = User::with('roles')->get();
+
+    $user = User::where('id', 2)->first();
+    if ($user) $user->removeRole('officer');
+
+    return response()->json('Sukses');
+});
